9c84cc363544b4352ee21d0e982103b794463e33
Tomasz Aniolowski <aniolowski93@gmail.com>
2019-07-06 12:09:40 +0200

    create DbBookService class

--- src/main/java/pl/coderslab/model/DbBookService.java
+++ src/main/java/pl/coderslab/model/DbBookService.java
@@ -1,4 +1,43 @@
 package pl.coderslab.model;
 
-public class DbBookService {
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DbBookService implements BookService {
+
+    private BookDao bookDao = new BookDao();
+
+    @Override
+    public List<Book> getList() {
+        List<Model> models = bookDao.loadAll();
+        List<Book> books = new ArrayList<>();
+        for (Model model : models) {
+            books.add((Book) model);
+        }
+        return books;
+    }
+
+    @Override
+    public Book getBook(Long id) {
+        return (Book) bookDao.loadById(id);
+    }
+
+    @Override
+    public Book addBook(Book book) {
+        bookDao.save(book);
+        return bookDao.loadByIsbn(book.getIsbn());
+    }
+
+    @Override
+    public void removeBook(Book book) {
+        bookDao.delete(book);
+    }
+
+    @Override
+    public void updateBook(Book book) {
+        bookDao.save(book);
+    }
 }
